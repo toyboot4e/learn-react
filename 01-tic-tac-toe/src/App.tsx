1fb8711f43ad8080a18b2c78a013c161835f86b3
toyboot4e <toyboot4e@gmail.com>
2024-01-30 22:45:11 +0900

    return multiple JSX elements, but inside a =div=

--- 01-tic-tac-toe/src/App.tsx
+++ 01-tic-tac-toe/src/App.tsx
@@ -4,6 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
 }
 
