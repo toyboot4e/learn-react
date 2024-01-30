28a8529693a5212384eaf30d577378cfd5776477
toyboot4e <toyboot4e@gmail.com>
2024-01-30 23:12:11 +0900

    mark a cell on click

--- 01-tic-tac-toe/src/App.tsx
+++ 01-tic-tac-toe/src/App.tsx
@@ -1,31 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function Square({ value }: { value: string }) {
-  return <button className="square">{value}</button>;
+function Square() {
+  // `useState` looks like assining a field (..but where? To the global context?)
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>{value}</button>
+  );
 }
 
 export default function Board() {
-  // TODO: flatten??
   return (
     <>
-      y-x
       <div className="board-row">
-        <Square value="1-1"></Square>
-        <Square value="1-2"></Square>
-        <Square value="1-3"></Square>
+        <Square />
+        <Square />
+        <Square />
       </div >
       <div className="board-row">
-        <Square value="2-1"></Square>
-        <Square value="2-2"></Square>
-        <Square value="2-3"></Square>
+        <Square />
+        <Square />
+        <Square />
       </div >
       <div className="board-row">
-        <Square value="3-1"></Square>
-        <Square value="3-2"></Square>
-        <Square value="3-3"></Square>
+        <Square />
+        <Square />
+        <Square />
       </div >
     </>
   );
