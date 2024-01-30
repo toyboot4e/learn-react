082b0e6cbecc8164b6d3e278e4c1d5f8a4e0e2de
toyboot4e <toyboot4e@gmail.com>
2024-01-30 21:13:15 +0900

    add `01-tic-tac-toc`

---
+++ 01-tic-tac-toe/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
