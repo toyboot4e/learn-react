679cfa080c4d9ee07df52754488bfa6a3383c638
toyboot4e <toyboot4e@gmail.com>
2024-01-31 21:14:32 +0900

    wip `Game` component

--- 01-tic-tac-toe/src/App.tsx
+++ 01-tic-tac-toe/src/App.tsx
@@ -37,7 +37,7 @@ function calcWinner(squares: string[]) {
   return null;
 }
 
-export default function Board() {
+function Board() {
   // TODO: use enumeartion type
   const [turnOfX, setTurnOfX] = useState(true);
 
@@ -101,3 +101,15 @@ export default function Board() {
   );
 }
 
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
