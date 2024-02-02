e700bf691b877d44a177877eb5f501e950b31f09
toyboot4e <toyboot4e@gmail.com>
2024-02-02 21:27:52 +0900

    why unique key warning?:
    Warning: Each child in a list should have a unique "key" prop.
    
    Check the render method of `Board`. See https://reactjs.org/link/warning-keys for more information.
    Square@http://localhost:5173/src/App.tsx?t=1706876834808:21:16
    Board@http://localhost:5173/src/App.tsx?t=1706876834808:49:15
    div
    div
    Game@http://localhost:5173/src/App.tsx?t=1706876834808:96:41

--- 01-tic-tac-toe/src/App.tsx
+++ 01-tic-tac-toe/src/App.tsx
@@ -78,26 +78,21 @@ function Board({ turnOfX, squares, onPlay }: BoardProps) {
     status = "Next player: " + (turnOfX ? "X" : "O");
   }
 
-  // TODO: use map
+  // TODO: opnionated formatter?
   return (
     <>
-      <div className="status">{status}<div />
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div >
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div >
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div >
-      </div >
+      <div className="status">{status}</div>
+      {
+        [0, 1, 2].map((row) =>
+          <div className="board-row">
+            {
+              [0, 1, 2].map((col) => {
+                var i = row * 3 + col;
+                return (<Square value={squares[i]} onSquareClick={() => handleClick(i)} />);
+              })
+            }
+          </div>)
+      }
     </>
   );
 }
