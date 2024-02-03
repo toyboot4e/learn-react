28793f0fb246515a1cc034f8d2bc0ece6089dc9d
toyboot4e <toyboot4e@gmail.com>
2024-02-03 10:24:34 +0900

    set titles

--- 02-learn-emmet/src/App.tsx
+++ 02-learn-emmet/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Emmet 速習</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
