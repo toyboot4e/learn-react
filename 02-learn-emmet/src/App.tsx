459be1e751f01b095516ac1f58bf6e4d4422969c
toyboot4e <toyboot4e@gmail.com>
2024-02-03 19:54:37 +0900

    prettier

--- 02-learn-emmet/src/App.tsx
+++ 02-learn-emmet/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import expand from 'emmet';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -12,15 +12,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p style={{ whiteSpace: "pre_line" }}>
-          {expand('div>ol>li+li+li')}
-        </p>
+        <p style={{ whiteSpace: 'pre_line' }}>{expand('div>ol>li+li+li')}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
