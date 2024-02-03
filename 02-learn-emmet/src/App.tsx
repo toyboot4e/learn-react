6a1f1c1cdcbd0f199d59d9027d46f5f48e5464b3
toyboot4e <toyboot4e@gmail.com>
2024-02-03 23:18:02 +0900

    `expand` is working

--- 02-learn-emmet/src/App.tsx
+++ 02-learn-emmet/src/App.tsx
@@ -1,22 +1,11 @@
-import { useState } from 'react';
 import './App.css';
 import expand from 'emmet';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Emmet 速習</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p style={{ whiteSpace: 'pre_line' }}>{expand('div>ol>li+li+li')}</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <textarea>{expand('div>ol>li+li+li')}</textarea>
     </>
   );
 }
